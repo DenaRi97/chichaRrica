8886224b853ba0a4bc49f9568ac04305e1cbac8f
Carla Escobar <carlaprimola@outlook.es>
2023-12-20 18:48:19 +0100

    añadiendo notas en gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,7 @@
 const URL = "http://localhost:3000/data";
 const photos = document.querySelector(".gallery__fotos");
 const buttonCategory = document.querySelector(".gallery__buttonCategory");
-const containerSubCategory = document.querySelector(
-  ".gallery_filtersSubCategory"
-);
+const containerSubCategory = document.querySelector(".gallery_filtersSubCategory");
 const buttonSubCategory = document.querySelector(".gallery__buttonSubCategory");
 
 // Variables para la paginación
@@ -30,14 +28,14 @@ fetch(URL)
       image.src = sourceImage;
       image.dataset.price = item.price;
 
-      image.addEventListener("click", function () {
+      image.addEventListener("click", function () { //cuando haga clic en una img abre el modal
         showModal(item);
       });
 
       return image;
     });
 
-    photos.append(...galeria);
+    photos.append(...galeria); //muestra fotos en galeria
   })
   .catch((error) => console.log("La hemos cagado"));
 
@@ -54,7 +52,7 @@ function showModal(item) {
   mainHeader.style.display = "none";
 }
 
-// Obtén referencias a los elementos del modal
+// Referencias a los elementos del modal
 const modal = document.getElementById("myModal");
 const modalImage = document.getElementById("modalImage");
 const modalTitle = document.getElementById("modalTitle");
@@ -116,104 +114,17 @@ buttonCategory.addEventListener("click", function () {
   }
 });
 
-function hideAllButtons(container) {
+function hideAllButtons(container) { //funcion para ocultar todos los botones
   const buttons = container.querySelectorAll("button");
   buttons.forEach((button) => {
     button.style.display = "none";
   });
 }
 
-function showAllButtons(container) {
-  const buttons = container.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.style.display = "block";
-  });
-}
-
-// Filtrar API por subcategorías ---------------------------------------------------------
-
-document.addEventListener("DOMContentLoaded", function () {
-  const buttons = document.querySelectorAll(".buttonSubCategory"); //botones subcategoría
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", function (event) {
-      const keyword = event.currentTarget.getAttribute("data-keyword");
-
-      fetch(URL)
-        .then((response) => response.json())
-        .then((data) => {
-          const matchingItems = data.filter((item) => item.keyword === keyword);
-
-          if (matchingItems.length > 0) {
-            console.log("Resultados para", keyword, ":", matchingItems);
-            showAllImages(matchingItems);
-          }
-        })
-        .catch((error) => console.error("Houston tenemos un problema:", error));
+function showAllButtons(container) { //funcion para mostrar todos los botones
+    
+    const buttons = container.querySelectorAll("button");
+    buttons.forEach(button => {
+        button.style.display = 'block';
     });
-  });
-});
-
-// Ajustar altura de forma automática --------------------------------------------------------
-
-// Guarda la altura original al cargar la página
-const alturaOriginal = document.querySelector(".gallery__fotos").offsetHeight;
-
-// Función para ajustar la altura de las imágenes
-function ajustarAltura() {
-  const galeria = document.querySelector(".gallery__fotos");
-
-  // Lógica para ajustar la altura según la subcategoría o filtro aplicado
-  // Puedes adaptar esto según cómo obtienes y aplicas los datos de la API
-  // Aquí un ejemplo básico:
-  const subcategoriaSeleccionada = obtenerSubcategoriaSeleccionada(); // Implementa esta función
-
-  if (subcategoriaSeleccionada) {
-    // Lógica para ajustar la altura según la subcategoría
-    galeria.style.columnCount = 2; // o el número que desees para la subcategoría
-  } else {
-    // Restablece a la altura original si no hay filtro
-    galeria.style.columnCount = "auto";
-  }
 }
-
-// Llama a la función al cargar la página y cuando se aplique el filtro
-document.addEventListener("DOMContentLoaded", ajustarAltura);
-document.addEventListener("cambioFiltro", ajustarAltura); // Escucha un evento personalizado o ajusta según cómo aplicas los filtros
-
-// Función ficticia para obtener la subcategoría seleccionada
-function obtenerSubcategoriaSeleccionada() {
-  // Implementa la lógica necesaria para obtener la subcategoría seleccionada
-  // Puede depender de cómo has estructurado tu página y cómo obtienes la información de los filtros
-   
-  return null; // Cambia esto según tu implementación
-}
-
-// Función para mostrar todas las imágenes
-function showAllImages(images) {
-  photos.innerHTML = "";
-
-  images.forEach((item) => {
-    const imgElement = document.createElement("img");
-    imgElement.src = item.url;
-    photos.appendChild(imgElement);
-  });
-}
-//Añado el boton del carrito
-addToCartButton.addEventListener("click", function () {
-  const productoSeleccionado = {
-      id: data[currentIndex].id,
-      title: data[currentIndex].title,
-      url: data[currentIndex].url,
-      price: data[currentIndex].price,
-  };
-
-  agregarProductoAlCarrito(productoSeleccionado);
-
-  modal.style.display = "none";
-  const mainHeader = document.getElementById("main-header");
-  mainHeader.style.display = "block";
-
-  // Redirección a carro.html
-  window.location.href = "carro.html";
-});
