7e2404ebc1bf22ffdc4b7a53f7e35e2b27bf2cfc
Almudena Rivera <riveraferalmudena@gmail.com>
2023-12-21 15:21:47 +0100

    REGISTRO FUNCIONA POR FIN

--- js/register.js
+++ js/register.js
@@ -1,56 +1,116 @@
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letters and spaces, up to 40 characters
-	password: /^.{4,12}$/, // Any character, 4 to 12 characters
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, // Email format
-  };
-  
-  const campos = {
-	nombre: false,
-	password: false,
-	correo: false,
-  };
-  
-  const validarCampo = (expresion, input, campo) => {
-	if (expresion.test(input.value)) {
-	  document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-	  document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-	  document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-	  document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-	  document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-	  campos[campo] = true;
-	} else {
-	  document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-	  document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-	  document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-	  document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-	  document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-	  campos[campo] = false;
-	}
-  };
-  
-  const validarFormulario = (event) => {
-	// Validate each field on keyup or blur
-	validarCampo(expresiones.nombre, document.getElementById('nombre'), 'nombre');
-	validarCampo(expresiones.password, document.getElementById('password'), 'password');
-	validarCampo(expresiones.correo, document.getElementById('correo'), 'correo');
-  }
-
-  const formulario = document.querySelector('.formulario form');
-  formulario.addEventListener('submit', validarFormulario);
-
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
 	var formulario = document.querySelector('.formulario form');
   
 	formulario.addEventListener('submit', function (event) {
 	  // Validaciones antes de enviar el formulario
-	  if (!validarCampo(expresiones.nombre, document.getElementById('nombre'), 'nombre') ||
-		  !validarCampo(expresiones.password, document.getElementById('password'), 'password') ||
-		  !validarCampo(expresiones.correo, document.getElementById('correo'), 'correo')) {
+	  if (!validarEmail() || !validarPassword()) {
 		// Si alguna validación falla, evitamos el envío del formulario
 		event.preventDefault();
 	  }
 	});
+  
+	function validarEmail() {
+	  var email = document.getElementById('correo').value.trim();
+	  if (email === '') {
+		alert('Ingresa un correo electrónico válido.');
+		return false;
+	  }
+  
+	  // Validar el formato del correo electrónico
+	  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	  if (!emailRegex.test(email)) {
+		alert('Correo electrónico no válido.');
+		return false;
+	  }
+  
+	  return true;
+	}
+  
+	function validarPassword() {
+	  var password = document.getElementById('password').value.trim();
+	  if (password === '') {
+		alert('Por favor, ingresa tu contraseña.');
+		return false;
+	  }
+  
+	  // Validar la fortaleza de la contraseña (puedes personalizar estos criterios)
+	  var strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+	  if (!strongPasswordRegex.test(password)) {
+		alert('La contraseña debe contener al menos 8 caracteres, una letra minúscula, una letra mayúscula y un número.');
+		return false;
+	  }
+  
+	  return true;
+	}
+
+	function validarName() {
+		var nombre = document.getElementById('nombre').value.trim();
+		if (nombre === '') {
+		  alert('Por favor, ingresa tu nombre.');
+		  return false;
+		}
+  
+		return true;
+	  }
   });
+  
+
+
+
+
+// const expresiones = {
+// 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letters and spaces, up to 40 characters
+// 	password: /^.{4,12}$/, // Any character, 4 to 12 characters
+// 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, // Email format
+//   };
+  
+//   const campos = {
+// 	nombre: false,
+// 	password: false,
+// 	correo: false,
+//   };
+  
+//   const validarCampo = (expresion, input, campo) => {
+// 	if (expresion.test(input.value)) {
+// 	  document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+// 	  document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+// 	  document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+// 	  document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+// 	  document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+// 	  campos[campo] = true;
+// 	} else {
+// 	  document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+// 	  document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+// 	  document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+// 	  document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+// 	  document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+// 	  campos[campo] = false;
+// 	}
+//   };
+  
+//   const validarFormulario = (event) => {
+// 	// Validate each field on keyup or blur
+// 	validarCampo(expresiones.nombre, document.getElementById('nombre'), 'nombre');
+// 	validarCampo(expresiones.password, document.getElementById('password'), 'password');
+// 	validarCampo(expresiones.correo, document.getElementById('correo'), 'correo');
+//   }
+
+//   const formulario = document.querySelector('.formulario form');
+//   formulario.addEventListener('submit', validarFormulario);
+
+//   document.addEventListener('DOMContentLoaded', function () {
+// 	var formulario = document.querySelector('.formulario form');
+  
+// 	formulario.addEventListener('submit', function (event) {
+// 	  // Validaciones antes de enviar el formulario
+// 	  if (!validarCampo(expresiones.nombre, document.getElementById('nombre'), 'nombre') ||
+// 		  !validarCampo(expresiones.password, document.getElementById('password'), 'password') ||
+// 		  !validarCampo(expresiones.correo, document.getElementById('correo'), 'correo')) {
+// 		// Si alguna validación falla, evitamos el envío del formulario
+// 		event.preventDefault();
+// 	  }
+// 	});
+//   });
   
 
 // const validarCampo = (expresion, input, campo) => {
